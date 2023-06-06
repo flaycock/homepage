23c67699a2752cb5bf11ab36984b67be2258aeff
flaycock <freddie.laycock@avnet.com>
2023-06-06 14:50:59 +0100

    Added eslint and prettier and have run both

--- personal-homepage/src/Projects.jsx
+++ personal-homepage/src/Projects.jsx
@@ -1,14 +1,17 @@
-import { Heading, Text } from '../node_modules/@chakra-ui/react';
+import { Heading, Text } from "../node_modules/@chakra-ui/react";
 
 const Projects = () => {
-    return (
-        <>
-            <Heading pb={30} size='2xl'>Projects</Heading>
-            <Text>
-              This section contains a few of my personal projects that I would like to share, along with the GitHub links to the source code.
-            </Text>
-        </>
-    );
-}
+  return (
+    <>
+      <Heading pb={30} size="2xl">
+        Projects
+      </Heading>
+      <Text>
+        This section contains a few of my personal projects that I would like to
+        share, along with the GitHub links to the source code.
+      </Text>
+    </>
+  );
+};
 
 export default Projects;
