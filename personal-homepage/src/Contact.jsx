f0d88d0779cfbf73f730262a25a31108fcf349ba
flaycock <freddie.laycock@avnet.com>
2023-06-06 14:23:56 +0100

    Adding some base info and styling to the pages

---
+++ personal-homepage/src/Contact.jsx
@@ -0,0 +1,20 @@
+import { Heading, Text, Link } from '../node_modules/@chakra-ui/react';
+
+const Contact = () => {
+    return (
+        <>
+            <Heading pb={30} size='2xl'>Contact</Heading>
+            <Text>
+              For any professional enquiries, please contact me on:<br />
+              <br />Email:&nbsp;
+              <Link _hover={{ color: 'gray.900', bg: 'green.200', borderRadius: '5px', p: '3px' }} target='_blank' href='mailto:freddielaycock97@gmail.com'>freddielaycock97@gmail.com</Link><br />
+              <br /> Alternatively, check out my&nbsp;
+                <Link _hover={{ color: 'gray.900', bg: 'green.200', borderRadius: '5px', p: '3px' }} target='_blank' href='https://www.linkedin.com/in/freddie-laycock-a23974171/'>LinkedIn</Link>
+              &nbsp;or my&nbsp;
+                <Link _hover={{ color: 'gray.900', bg: 'green.200', borderRadius: '5px', p: '3px' }} target='_blank' href='https://github.com/flaycock'>GitHub profile</Link>
+            </Text>
+        </>
+    );
+}
+
+export default Contact;
--- personal-homepage/src/Projects.jsx
+++ personal-homepage/src/Projects.jsx
@@ -3,9 +3,9 @@ import { Heading, Text } from '../node_modules/@chakra-ui/react';
 const Projects = () => {
     return (
         <>
-            <Heading pb='20px' size='lg' fontSize='50px'>Projects</Heading>
+            <Heading pb={30} size='2xl'>Projects</Heading>
             <Text>
-              This sections contains a few of my personal projects that I would like to share, along with the GitHub links to the source code.
+              This section contains a few of my personal projects that I would like to share, along with the GitHub links to the source code.
             </Text>
         </>
     );
