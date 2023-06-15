57d192ece594ac3a29b5c319ea10840db788f497
flaycock <freddie.laycock@avnet.com>
2023-06-15 09:49:32 +0100

    Added background image and few other tweaks

--- personal-homepage/src/main.jsx
+++ personal-homepage/src/main.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ChakraProvider } from "../node_modules/@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "../node_modules/@chakra-ui/react";
 import "./index.css";
+
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: "",
+      },
+    }),
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
