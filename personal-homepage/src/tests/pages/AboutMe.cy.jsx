a256774e9ae0fe07c3b6000826b22e816acc926f
flaycock <91275370+flaycock@users.noreply.github.com>
2025-06-14 11:50:23 +0100

    Delete personal-homepage directory

--- personal-homepage/src/tests/pages/AboutMe.cy.jsx
+++
@@ -1,8 +0,0 @@
-import AboutMe from "../components/AboutMe";
-
-describe("<AboutMe />", () => {
-  it("renders", () => {
-    cy.mount(<AboutMe />);
-    cy.get("h2.aboutMeHeading").should("have.text", "About Me");
-  });
-});
--- personal-homepage/src/tests/projects/CatPetter.cy.jsx
+++
@@ -1,8 +0,0 @@
-import CatPetter from "../../projects/CatPetter/CatPetter";
-
-describe("<CatPetter />", () => {
-  it("renders", () => {
-    cy.mount(<CatPetter />);
-    cy.get("#catPetterHeader").should("have.text", "Cat Petter");
-  });
-});
